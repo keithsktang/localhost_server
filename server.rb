a1c87316cddb6823ede5ceda0931f9dcc819e594
Keith Tang <keithsktang@gmail.com>
2015-10-26 14:12:48 -0400

    Change profolio directory to root

--- server.rb
+++ server.rb
@@ -1,7 +1,13 @@
 require 'sinatra'
 
-get "/" do
-  File.read(File.join('public', 'index.html'))
+# set :public_folder, 'public'
+#
+# get "/" do
+#   redirect '/subfolder/index.html'
+# end
+
+get '/' do
+  send_file File.join(settings.public_folder, 'index.html')
 end
 
 get "/sinatra" do
