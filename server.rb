f31bb734b843ab802b3587fcf78a023dbe2d998e
Keith Tang <keithsktang@gmail.com>
2015-10-26 17:57:46 -0400

    clean up public folders

--- server.rb
+++ server.rb
@@ -1,15 +1,6 @@
 require 'sinatra'
 
-# set :public_folder, 'public'
-#
-# get "/" do
-#   redirect '/subfolder/index.html'
-# end
 
 get '/' do
   send_file File.join(settings.public_folder, 'index.html')
-end
-
-get "/sinatra" do
-   "Hello Sinatra"
 end
